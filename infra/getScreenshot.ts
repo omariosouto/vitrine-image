354df362cbc282350562be3d07199213c0f47814
Mario Souto <soutomarios@gmail.com>
2021-01-29 20:22:07 -0300

    add trim

--- infra/getScreenshot.ts
+++ infra/getScreenshot.ts
@@ -48,8 +48,8 @@ async function getPage(): Promise<Page> {
 export async function getScreenshot(url: string, { width, height } = { width: 852, height: 480 }) {
   const page = await getPage();
 
-  
-  await page.goto(`https://${url.replace('http://', '').replace('https://', '')}`);
+
+  await page.goto(`https://${url.replace('http://', '').replace('https://', '').trim()}`);
   await page.waitForTimeout(3000);
   await page.setViewport({ width, height });
 
